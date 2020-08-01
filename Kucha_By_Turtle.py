bc1554d094a22f0f4d7fa8907afb8c1afba39723
Tohaman <avrozovu@yandex.ru>
2020-08-01 23:29:46 +0300

    Commit

--- Kucha_By_Turtle.py
+++ Kucha_By_Turtle.py
@@ -3,6 +3,7 @@ import random
 
 # kucha = random.randint(5, 25)
 kucha = 10
+radius = 15
 win = turtle.Screen()
 alex = turtle.Turtle()
 alex.speed("fastest")
@@ -16,9 +17,9 @@ while kucha > max_in_kucha:
 alex.write(str(kucha))
 alex.penup()
 alex.right(90)
-alex.fd(row_count * 40)
+alex.fd(row_count * radius * 2)
 alex.right(90)
-alex.fd(row_count * 20)
+alex.fd(row_count * radius)
 alex.left(180)
 
 alex.pendown()
@@ -27,17 +28,17 @@ while output_count < kucha:
     output_in_row = 0
     while output_in_row < row_count and output_count < kucha:
         alex.pendown()
-        alex.circle(20)
+        alex.circle(radius)
         alex.penup()
-        alex.fd(40)
+        alex.fd(radius * 2)
         output_in_row += 1
         output_count += 1
     row_count -= 1
     alex.left(90)
-    alex.fd(36)
+    alex.fd(radius * 2)
     alex.left(90)
-    alex.fd(20)
-    alex.fd(40 * row_count)
+    alex.fd(radius)
+    alex.fd(radius * 2 * row_count)
     alex.left(180)
     alex.pendown()
 
