ce397a7f3e13ba2ee1842a20bf4951f818a24bf5
Tohaman <avrozovu@yandex.ru>
2020-11-06 18:58:41 +0300

    Commit

--- Miron/procudure.py
+++ Miron/procudure.py
@@ -4,11 +4,20 @@ def custom_print(n):
     print("-     ", n, "      -")
     print("________________")
 
+def print_discriminant():
+    d = b ** 2 - 4 * a * c
+    print(d)
 
 custom_print(3)
 n = 5
 custom_print(n)
 a = 8
-custom_print(a)
+b = 3
+c = 5
+d = 0
+# custom_print(a)
 # print(i)
+# d = b ** 2 - 4 * a * c
+# print(d)
+print_discriminant()
 
