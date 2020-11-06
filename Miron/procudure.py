1dd6f35d89ab1e099845715c1ae4b6876b4fd9e5
Tohaman <avrozovu@yandex.ru>
2020-11-06 18:52:24 +0300

    Commit

---
+++ Miron/procudure.py
@@ -0,0 +1,14 @@
+def custom_print(n):
+    i = n * 2
+    print("----------------")
+    print("-     ", n, "      -")
+    print("________________")
+
+
+custom_print(3)
+n = 5
+custom_print(n)
+a = 8
+custom_print(a)
+# print(i)
+
