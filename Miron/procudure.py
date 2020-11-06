4feec872c844f7430d09daf423dbee046388c74f
Tohaman <avrozovu@yandex.ru>
2020-11-06 19:05:19 +0300

    Commit

--- Miron/procudure.py
+++ Miron/procudure.py
@@ -4,20 +4,21 @@ def custom_print(n):
     print("-     ", n, "      -")
     print("________________")
 
-def print_discriminant():
-    d = b ** 2 - 4 * a * c
-    print(d)
+def print_volume():
+    pi = 3.1415
+    v = pi * r * r * h
+    print(v)
 
 custom_print(3)
 n = 5
 custom_print(n)
-a = 8
-b = 3
-c = 5
-d = 0
+r = 3
+h = 5
 # custom_print(a)
 # print(i)
-# d = b ** 2 - 4 * a * c
-# print(d)
-print_discriminant()
+
+# pi = 3.14
+# v = pi * r * r * h
+# print(v)
+print_volume()
 
